fdf3e4eb1ae414171b9dcb179c36fc40a8545aa4
Félix Girault <felix.girault@gmail.com>
2026-01-08 18:08:42 +0100

    Using smaller timeouts on e2e tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,7 +4,10 @@ export default defineConfig({
 	testDir: './e2e',
 	fullyParallel: true,
 	forbidOnly: !!process.env.CI,
-	retries: process.env.CI ? 2 : 0,
+	timeout: 5_000,
+	expect: {
+		timeout: 1_000
+	},
 	workers: process.env.CI ? 1 : undefined,
 	reporter: 'list',
 	use: {
